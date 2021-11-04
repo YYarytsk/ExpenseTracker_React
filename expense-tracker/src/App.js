330c2c941a78b534909335a459a32a8ede9e5b1c
YYarytsk <iamfromukraine@gmail.com>
2021-11-03 23:59:20 -0400

    styled ExpenseItem component

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -4,22 +4,12 @@ import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      
+       
         <ExpenseItem></ExpenseItem>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      
     </div>
   );
 }
